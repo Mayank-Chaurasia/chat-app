66b57ff5039d1ebcc8d122b1fa5b3e6ee768ee3b
Mayank-Chaurasia <69709616+Mayank-Chaurasia@users.noreply.github.com>
2025-09-03 16:14:04 +0000

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {HashRouter} from 'react-router-dom';
 import './index.scss';
@@ -9,11 +8,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <>
       <HashRouter>
           <App />
       </HashRouter>
-  </React.StrictMode>
+  </>
 );
 
 reportWebVitals();
